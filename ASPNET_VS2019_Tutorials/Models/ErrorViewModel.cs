bc504a995f9629e139804e3375a2c9938670e51d
PedroSequeira <pedromiguel_162@hotmail.com>
2023-03-16 17:05:30 +0000

    new MVC project created

---
+++ ASPNET_VS2019_Tutorials/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNET_VS2019_Tutorials.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
