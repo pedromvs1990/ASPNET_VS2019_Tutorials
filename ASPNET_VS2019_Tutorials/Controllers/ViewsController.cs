ad81f794da20bbe4a5c3fae776a645f475fca0e9
PedroSequeira <pedromiguel_162@hotmail.com>
2023-03-19 16:14:59 +0000

    Lesson 4: Views

---
+++ ASPNET_VS2019_Tutorials/Controllers/ViewsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNET_VS2019_Tutorials.Controllers
+{
+    public class ViewsController : Controller
+    {
+
+        //Lesson 4: Views
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public IActionResult Index1()
+        {
+            ViewData["Message"] = "Obs: this is Index1 Page";
+            return View("Index");
+        }
+
+        public IActionResult Index2()
+        {
+            ViewData["Message"] = "Obs: this is Index2 Page";
+            return View("Index");
+        }
+
+        public IActionResult Index3()
+        {
+            ViewData["Message"] = "Obs: this is Index3 Page";
+            return View();
+
+        }
+
+        public IActionResult Index4()
+        {
+            return View();
+        }
+    }
+}
