b67e86ee1a8f2a6ffd7bb0aaeee7c6882c022ed7
PedroSequeira <pedromiguel_162@hotmail.com>
2023-03-19 19:12:07 +0000

    Lesson 4: Standard HTML, HTML Helper and TAG Helper

---
+++ ASPNET_VS2019_Tutorials/Controllers/HelpersController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNET_VS2019_Tutorials.Controllers
+{
+
+    //Lesson 4: Standard HTML href ; HTML and TAG Helpers
+    public class HelpersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult StandardHTML()
+        {
+            return View();
+        }
+
+        public IActionResult StandardHTML_Param()
+        {
+            return View();
+        }
+        public IActionResult HTMLHelper()
+        {
+            return View();
+        }
+        public IActionResult HTMLHelper_Param()
+        {
+            return View();
+        }
+
+        public IActionResult TagHelper()
+        {
+            return View();
+        }
+
+        public IActionResult TagHelper_Param()
+        {
+            return View();
+        }
+
+    }
+}
