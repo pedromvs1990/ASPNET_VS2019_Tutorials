b4178cbef13d0dd10ca2faf32d6de0faf17e626f
PedroSequeira <pedromiguel_162@hotmail.com>
2023-03-19 23:23:45 +0000

    Lesson 4: Partial Views

---
+++ ASPNET_VS2019_Tutorials/Controllers/PartialViewController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNET_VS2019_Tutorials.Controllers
+{
+    public class PartialViewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
